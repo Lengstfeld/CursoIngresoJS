1350c427c19d7ffa24cf3d6971a0be0108b4b3c6
Lengstfeld <lengstfeld@gmail.com>
2020-07-13 01:14:38 -0300

    Terminado

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,32 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largo = parseInt(txtIdLargo.value);
+    var ancho = parseInt(txtIdAncho.value);
+    var radio = parseInt(txtIdRadio.value);
+
+    var sumar = ((largo + ancho)* 2 )* 3;
+    alert(`Se necesita ${sumar}m. de alambre`)
 
 }
 function Circulo () 
 {
+    var largo = parseInt(txtIdLargo.value);
+    var ancho = parseInt(txtIdAncho.value);
+    var radio = parseInt(txtIdRadio.value);
+    var pi = 3.14;
+
+    var areaCirculo = ((radio * radio)*pi) *3;
+    alert(`Se necesita ${areaCirculo}m. de alambre`)
 	
 }
 function Materiales () 
 {
-	
+    var largo = parseInt(txtIdLargo.value);
+    var ancho = parseInt(txtIdAncho.value);
+    var radio = parseInt(txtIdRadio.value);
+    var cemento = (largo * ancho) * 2
+    var cal = (largo * ancho) * 3
+    
+    alert(`Se necesita ${cemento} bolsas de cemento y ${cal} de cal`)
 }
