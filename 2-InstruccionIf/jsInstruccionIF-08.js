e85dfa1e62740d7ebd9926a62f51da5984d927d4
Lengstfeld <lengstfeld@gmail.com>
2020-07-23 20:43:31 -0300

    Terminado

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,12 +1,8 @@
 function mostrar()
 {
-	let idEdad;
-	let idEstado;
 	let estado;
 	let edad;
-	idEdad = document.getElementById("txtIdEdad").value
-	idEstado = document.getElementById("estadoCivil").value
-	estado = idEstado;
-	edad = parseInt(idEdad);
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estado = document.getElementById("estadoCivil").value;
 	if(edad >= 18 && estado == "Soltero"){alert('Es soltero y no es menor.');}
 }//FIN DE LA FUNCIÓN
