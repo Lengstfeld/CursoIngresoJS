18b4b8cfd07e2fb329ea06d2b508e24c2bb9783f
Engstfeld, Leandro German <lengstfeld@gmail.com>
2020-07-22 22:26:19 -0300

    Terminado

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -4,7 +4,5 @@ function mostrar()
 	let edad;
 	idEdad = document.getElementById("txtIdEdad").value;
 	edad = parseInt(idEdad);
-	if( edad <= 12){alert("No es adolecente");}
-	else if(edad >= 13 && edad <= 17 ){alert("Es adolecente");}
-	else{alert("No es adolecente");};
+	if( edad <= 12 || edad >= 18 ){alert("No es adolecente");}
 }//FIN DE LA FUNCIÓN
