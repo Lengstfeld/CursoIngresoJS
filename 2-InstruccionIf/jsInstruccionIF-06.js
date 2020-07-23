8003bfbfca578173ffcab5d3aaef6fb76612a975
Lengstfeld <lengstfeld@gmail.com>
2020-07-23 19:41:09 -0300

    Terminado

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -4,8 +4,8 @@ function mostrar()
 	let edad;
 	idEdad = document.getElementById("txtIdEdad").value;
 	edad = parseInt(idEdad);
-	if(edad <= 12){alert("Es un niñ@");}
-	else if(edad >= 13 && edad <= 17 ){alert("Es adolecente");}
+	if(edad < 13){alert("Es un niñ@");}
+	else if(edad < 18){alert("Es adolecente");}
 	else{alert("Es mayor de edad");};
 
 }//FIN DE LA FUNCIÓN
