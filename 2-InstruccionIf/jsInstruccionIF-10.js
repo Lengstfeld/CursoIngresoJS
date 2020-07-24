6e9d6de2517bb30423a62c86b2c9f12abe2363a9
Lengstfeld <lengstfeld@gmail.com>
2020-07-23 22:34:36 -0300

    Terminado

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -3,7 +3,7 @@ function mostrar()
 	let numero;
 	numero = Math.floor(Math.random() * 10) + 1;
 	if(numero < 4) {alert(`Vamos, la proxima se puede ${numero}`);}
-	else if(numero > 4 && numero < 9) {alert(`APROBÓ ${numero}`);}
+	else if(numero >= 4 && numero < 9) {alert(`APROBÓ ${numero}`);}
 	else {alert(`EXCELENTE ${numero}`);}
 
 }//FIN DE LA FUNCIÓN
